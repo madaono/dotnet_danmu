d0ac29024a53fc4ff2e84e64a90f7e6c702a122b
madaono <wangzhejingling@gmail.com>
2019-07-10 16:50:51 +0800

    [init] project init

---
+++ clientapp/src/Components/FetchTest.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class FetchTest extends React.Component {
+    constructor(props:any){
+        super(props);
+    }
+    componentDidMount(){
+        fetch('/api/SampleData/WeatherForecasts')
+        .then(a => a.json())
+        .then(b=>console.log(b))
+    }
+
+    render() {
+        return (
+            <div>heelo</div>
+        )
+    }
+}
